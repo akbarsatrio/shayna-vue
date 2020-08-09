f3fff0810716d19939698966cba40dbc3128630e
Akbar Satrio <akbarsatrio@outlook.co.id>
2020-08-09 07:29:33 +0700

    Project Vue Laravel

---
+++ src/components/PartnerShayna.vue
@@ -0,0 +1,47 @@
+<template>
+	<div>
+		<!-- Partner Logo Section Begin -->
+	<div class="partner-logo">
+		<div class="container">
+			<carousel class="logo-carousel" :nav="0" :items="5" :dots="0" :autplay="true">
+				<div class="logo-item">
+					<div class="tablecell-inner">
+						<img src="../../public/img/logo-carousel/logo-1.png" alt="" />
+					</div>
+				</div>
+				<div class="logo-item">
+					<div class="tablecell-inner">
+						<img src="../../public/img/logo-carousel/logo-2.png" alt="" />
+					</div>
+				</div>
+				<div class="logo-item">
+					<div class="tablecell-inner">
+						<img src="../../public/img/logo-carousel/logo-3.png" alt="" />
+					</div>
+				</div>
+				<div class="logo-item">
+					<div class="tablecell-inner">
+						<img src="../../public/img/logo-carousel/logo-4.png" alt="" />
+					</div>
+				</div>
+				<div class="logo-item">
+					<div class="tablecell-inner">
+						<img src="../../public/img/logo-carousel/logo-5.png" alt="" />
+					</div>
+				</div>
+			</carousel>
+		</div>
+	</div>
+	<!-- Partner Logo Section End -->
+	</div>
+</template>
+
+<script>
+import carousel from 'vue-owl-carousel'
+export default {
+	name: 'PartnerShayna',
+	components: {
+		carousel
+	}
+}
+</script>
